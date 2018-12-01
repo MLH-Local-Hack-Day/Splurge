e180aa9e74eaa2b78dc3c8583dbb8c75288ae655
Mauricio Chávez <mauriciochavez@ciencias.unam.mx>
2018-12-01 12:17:13 -0600

    Added directories and a few settings

--- splurge/splurge/settings.py
+++ splurge/splurge/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'splurge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/shop/'
+LOGOUT_REDIRECT_URL = '/shop/'
