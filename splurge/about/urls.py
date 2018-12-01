7aa57088d387000a48589b9df75d48e7327579d2
Mario <mari@MarioFCUNAM.MarioFCUNAM.com>
2018-12-01 16:39:13 -0600

    urls

--- splurge/about/urls.py
+++ splurge/about/urls.py
@@ -6,4 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('crearcuenta/', views.Descubremas1, name='Descubremas1'),
+    path('acomularpuntos/', views.Descubremas2, name='Descubremas2'),
+    path('organizador/', views.Descubremas3, name='Descubremas3'),
+    path('codigo/', views.Descubremas4, name='Descubremas4'),
 ]
