735b3e07f36e23ea33ed90246eca8c57e79f6efa
Mauricio Chávez <mauriciochavez@ciencias.unam.mx>
2018-12-01 17:21:52 -0600

    :star: Added authenticated view and links to index

--- splurge/about/urls.py
+++ splurge/about/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('crearcuenta/', views.descubremas1, name='Descubremas1'),
-    path('acomularpuntos/', views.descubremas2, name='Descubremas2'),
+    path('acumularpuntos/', views.descubremas2, name='Descubremas2'),
     path('organizador/', views.descubremas3, name='Descubremas3'),
     path('codigo/', views.descubremas4, name='Descubremas4'),
 ]
